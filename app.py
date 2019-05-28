e64e185c56b2aa76084c4bda3e0a61877ee9edb4
Le Aundre Jackson <dell.5287@gmail.com>
2019-05-28 18:05:51 -0400

    Update files

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,7 +14,19 @@ from models import Result
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    errors = []
+    results = {}
+    if request.method == "POST":
+        # get url that the user has entered
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+    return render_template('index.html', errors=errors, results=results)
 
 
 if __name__ == '__main__':

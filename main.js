ed6eb24dfa9d15e538e1128e949f3fcca981cb36
Le Aundre Jackson <dell.5287@gmail.com>
2019-06-20 12:40:10 -0400

    Created Files Add a JS file and update html

---
+++ main.js
@@ -0,0 +1,54 @@
+(function () {
+	'use strict';
+
+	angular.module('WordcountApp', [])
+
+		.controller('WordcountController', ['$scope', '$log',
+    function ($scope, $log) {
+				$scope.getResults = function () {
+					$log.log("test");
+				};
+    }
+  ]);
+
+}());
+
+.controller('WordcountController', ['$scope', '$log', '$http',
+function getWordCount(jobID) {
+		var timeout = "";
+
+		var poller = function () {
+			// fire another request
+			$http.get('/results/' + jobID)
+				.
+			success(function (data, status, headers, config) {
+				if (status === 202) {
+					$log.log(data, status);
+				} else if (status === 200) {
+					$log.log(data);
+					$scope.wordcounts = data;
+					$timeout.cancel(timeout);
+					return false;
+				}
+				// continue to call the poller() function every 2 seconds
+				// until the timeout is cancelled
+				timeout = $timeout(poller, 2000);
+			});
+		};
+		poller();
+}
+]);
+
+$http.post('/start', {
+		"url": userInput
+	})
+	.
+success(function (results) {
+		$log.log(results);
+		getWordCount(results);
+
+	})
+	.
+error(function (error) {
+	$log.log(error);
+});
